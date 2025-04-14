7430ec8407ab10caf10d334241326cd6437119ad
IsacPasianotto <yh4mop12z@gmail.com>
2025-04-14 11:38:23 +0200

    fixed duplicated entries with reuse

--- script/master_node/cpu/offline_core.sh
+++ script/master_node/cpu/offline_core.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
 
 # SPDX-FileCopyrightText: 2025 Isac Pasianotto <isac.pasianotto@phd.units.it>
-# SPDX-FileCopyrightText: 2025 Isac Pasianotto isac.pasianotto@phd.units.it
 # SPDX-FileCopyrightText: 2025 Niccolo Tosato <niccolo.tosato@phd.units.it>
-# SPDX-FileCopyrightText: 2025 Niccolo Tosato niccolo.tosato@phd.units.it
 # SPDX-FileCopyrightText: 2025 Ruggero Lot <ruggero.lot@areasciencepark.it>
-# SPDX-FileCopyrightText: 2025 opyright=Ruggero Lot ruggero.lot@areasciencepark.it
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
